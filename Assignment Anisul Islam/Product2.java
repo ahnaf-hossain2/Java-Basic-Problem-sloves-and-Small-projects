54f6c2820696e9eaf13d2e876eb0caa6f89fa769
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-02-01 18:15:37 +0600

    Add Product2 class with product details and output

---
+++ Assignment Anisul Islam/Product2.java
@@ -0,0 +1,16 @@
+public class Product2 {
+	public static void main (String[] args) {
+		byte id = 101;
+		String title, price, description, category;
+		title = "iphone 15,";
+		price = "1895 euros,";
+        description = "Perfect product with best image quality,";
+		category = "Phone.";
+
+		System.out.println("ID :"+id+",");
+		System.out.println(title);
+		System.out.println(price);
+		System.out.println(description);
+		System.out.println("Category: "+category);
+	}
+}
