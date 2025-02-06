cbf3d089709014b560a18ec8168cd3dd892468ce
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-02-06 11:42:03 +0600

    Optimize PrimeNum class to reduce iteration range for prime number checks

--- Problem Solves/PrimeNum.java
+++ Problem Solves/PrimeNum.java
@@ -10,7 +10,7 @@ public class PrimeNum {
         if (num == 0 || num == 1) {
             System.out.println("Not a prime number");
         } else {
-            for (int i = 2; i < num; ++i) {
+            for (int i = 2; i < num / 2; ++i) { // a number can never be divided by more than half of it's value
                 if (num % i == 0) {
                     System.out.println("Not a prime number");
                     count++;
