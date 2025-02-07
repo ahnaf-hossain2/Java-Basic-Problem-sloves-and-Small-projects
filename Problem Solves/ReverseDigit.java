d3455437d317d38e8afe862e56760d35d181b5ac
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-02-07 17:21:07 +0600

    Enhance ReverseDigit class to display original number before reversing

--- Problem Solves/ReverseDigit.java
+++ Problem Solves/ReverseDigit.java
@@ -4,15 +4,17 @@ public class ReverseDigit {
 	public static void main (String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		int temp;
-		int r, sum = 0;
+		int temp, r;
 		System.out.print("Enter the digits: ");
-		temp = input.nextInt();
-
-		while (temp != 0) {
-			r = temp % 10;
-			System.out.print(r);
-			temp /= 10;
-		}
+        temp = input.nextInt();
+        System.out.println("Original number: " + temp);
+        System.out.print("Reverse number: ");
+        while (temp != 0) {
+            r = temp % 10;
+            System.out.print(r);
+            temp /= 10;
+        }
+        
+        input.close();
 	}
 }
